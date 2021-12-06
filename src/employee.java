83861b7c2db51c477bcdd742b25d8208ef094c15
Daniil <byka763@gmail.com>
2021-12-06 14:11:28 +0300

    Initial commit

---
+++ src/employee.java
@@ -0,0 +1,14 @@
+public class employee {
+    public String fio;
+    public String post;
+    public String email;
+    public String telephone;
+    public int salary;
+    public int age;
+
+    public employee (String fio, String post, String email, String telephone, int salary, int age){
+
+    }
+}
+
+
